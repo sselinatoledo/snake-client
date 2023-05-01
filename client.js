012c1743b4b8ac332c62405dab54edcd28a83351
sselinatoledo <sselinatoledo@gmail.com>
2023-05-01 04:04:06 +0000

    update client.js

--- client.js
+++ client.js
@@ -1,12 +1,26 @@
 const net = require("net");
 
-// establishes a connection with the game server
+// Establishes a connection with the game server
 const connect = function () {
     const conn = net.createConnection({
         host: "10.0.2.15",
         port: 50541,
     });
+
+    // Interpret incoming data as text
+    conn.setEncoding("utf8");
+
+    conn.on("connect", () => {
+        console.log("Successfully connected to game server");
+        conn.write("Name: SLT");
+    })
+
+    // Event handler for incoming data
+    conn.on("data", (data) => {
+        console.log("Server says:", data);
+    });
+
     return conn;
 };
 
-module.exports = {connect};
+module.exports = { connect };
