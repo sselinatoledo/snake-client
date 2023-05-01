685bd0a0a9fea1e94feb9723efc00887e0a59ab2
sselinatoledo <sselinatoledo@gmail.com>
2023-05-01 06:05:13 +0000

    update play.js

--- play.js
+++ play.js
@@ -1,20 +1,7 @@
-const {connect} = require("./client")
+const {connect} = require("./client");
+const {setupInput} = require("./input");
 
 console.log("Connecting ...");
 connect();
 
-// setup interface to handle user input from stdin
-const setupInput = function () {
-    const stdin = process.stdin;
-    stdin.setRawMode(true);
-    stdin.setEncoding("utf8");
-    stdin.resume();
-    stdin.on("data", handleUserInput);
-    return stdin;
-  };
-
-  const handleUserInput = function () {
-    if (key === "\u0003") {
-        process.exit();
-    }
-  };
+setupInput();
