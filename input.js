124c28b21bcc2b4a774a6346c6fade26ca80f77b
sselinatoledo <sselinatoledo@gmail.com>
2023-05-01 06:05:34 +0000

    update input.js

---
+++ input.js
@@ -0,0 +1,16 @@
+// setup interface to handle user input from stdin
+const setupInput = function () {
+    const stdin = process.stdin;
+    stdin.setRawMode(true);
+    stdin.setEncoding("utf8");
+    stdin.resume();
+    stdin.on("data", handleUserInput);
+    return stdin;
+  };
+
+  const handleUserInput = function () {
+    if (key === "\u0003") {
+        process.exit();
+    }
+  };
+  module.exports = {setupInput};
