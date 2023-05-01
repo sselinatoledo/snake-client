834e4c8c89e6266dc99ffe8d2b04954e5dfea718
sselinatoledo <sselinatoledo@gmail.com>
2023-05-01 04:03:43 +0000

    update play.js

--- play.js
+++ play.js
@@ -1,19 +1,4 @@
-// establishes a connection with the game server
 const {connect} = require("./client")
 
-const conn = connect();
-    
-    // interpret incoming data as text
-    conn.setEncoding("utf8");
-
-    // event handler for incoming data
-    conn.on("data", (data) => {
-        console.log("Server says:", data);
-    });
-
-    // event handler for when the connection is first established
-    conn.on("connect", () => {
-        console.log("Connected to server!");
-    });
-
-    console.log("Connecting ...");
+console.log("Connecting ...");
+connect();
